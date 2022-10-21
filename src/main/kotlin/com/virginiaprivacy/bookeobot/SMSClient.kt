e6b32226e0b562f9d2281f807e6c2f0e21d24b0e
Virginia-Privacy-Coalition <contact@virginiaprivacy.com>
2022-10-20 21:14:12 -0400

    convert to cli program

--- src/main/kotlin/com/virginiaprivacy/bookeobot/SMSClient.kt
+++ src/main/kotlin/com/virginiaprivacy/bookeobot/SMSClient.kt
@@ -3,11 +3,11 @@ package com.virginiaprivacy.bookeobot
 
 abstract class SMSClient(val defaultOriginNumber: String) {
 
-    abstract fun sendMessage(destinationNumber: String, persistence: BookeoBot.Persistence, body: String)
-    abstract fun sendMessage(destinationNumber: String, persistence: BookeoBot.Persistence, originNumber: String, body: String)
-    abstract fun sendMessageToAllNumbers(event: Event, persistence: BookeoBot.Persistence)
-    abstract fun sendMessageToAllNumbers(event: Event, persistence: BookeoBot.Persistence, originNumber: String = defaultOriginNumber)
-    abstract fun sendMessageToAllNumbers(persistence: BookeoBot.Persistence, body: String)
+    abstract fun sendMessage(destinationNumber: String, persistence: Persistence, body: String)
+    abstract fun sendMessage(destinationNumber: String, persistence: Persistence, originNumber: String, body: String)
+    abstract fun sendMessageToAllNumbers(event: Event, persistence: Persistence)
+    abstract fun sendMessageToAllNumbers(event: Event, persistence: Persistence, originNumber: String = defaultOriginNumber)
+    abstract fun sendMessageToAllNumbers(persistence: Persistence, body: String)
 
 }
 
